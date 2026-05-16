03617b765353fb5fcbe23af48a677c5092dfe636
Saurlax <saurlax@qq.com>
2026-05-16 19:41:20 +0800

    chore: remove unused pgvector plugin initialization

--- server/plugins/init.ts
+++
@@ -1,6 +0,0 @@
-import { db } from "@nuxthub/db";
-import { sql } from "drizzle-orm";
-
-export default defineNitroPlugin(async () => {
-  await db.execute(sql`CREATE EXTENSION IF NOT EXISTS vector`);
-});
